b394682f7423126f19359f45e0b6387b6b371ad7
Pedro Moraes <pedromoraes005@gmail.com>
2025-07-29 00:29:53 -0300

    ajustes

--- src/app/organismos/dashboard/dashboard.module.ts
+++ src/app/organismos/dashboard/dashboard.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { DashboardRoutes } from './dashboard.routes';
+import { DashboardService } from './dashboard.service';
 
 @NgModule({
   declarations: [],
   imports: [DashboardRoutes, CommonModule],
   exports: [],
-  providers: [],
+  providers: [DashboardService],
 })
 export class DashboardModule {}
